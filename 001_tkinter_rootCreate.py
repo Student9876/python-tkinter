d222668835a532a052883c710d0fb37c32476ec7
Student9876 <Shouvik9876@gmail.com>
2022-11-09 23:34:03 +0530

    Tkinter root creation and mainloop

---
+++ 001_tkinter_rootCreate.py
@@ -0,0 +1,11 @@
+# It's not a good thing to import all the things in a library as it's done in the below 
+from tkinter import *
+
+# generally we type root = Tk()
+zeon_root = Tk() # here we have created a base. Now we can add things in this base
+
+
+# gui logic here
+
+zeon_root.mainloop() #mainloop keeps you in the gui window and make it interactive
+
